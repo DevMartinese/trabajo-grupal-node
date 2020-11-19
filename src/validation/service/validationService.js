43cf792fa515924992f9078c7fb60b6d665c7dca
Gonzalo Martinese <devmartinese@gmail.com>
2020-11-19 18:55:05 -0300

    'Ahora'

--- src/validation/service/validationService.js
+++ src/validation/service/validationService.js
@@ -2,7 +2,7 @@ const sqs = require("ebased/service/downstream/sqs");
 
 module.exports = async (payload) => {
   const sqsSendParams = {
-    MessageBody: payload,
+    MessageBody: payload.Payload,
     QueueUrl: process.env.QUEUE_URL,
   };
 
