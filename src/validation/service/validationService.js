216f594b70552f621143be95948d5bbc4bad59d5
Gonzalo Martinese <devmartinese@gmail.com>
2020-11-19 18:49:48 -0300

    'sdsad'

--- src/validation/service/validationService.js
+++ src/validation/service/validationService.js
@@ -2,9 +2,7 @@ const sqs = require("ebased/service/downstream/sqs");
 
 module.exports = async (payload) => {
   const sqsSendParams = {
-    MessageBody: {
-      Payload: payload,
-    },
+    MessageBody: payload,
     QueueUrl: process.env.QUEUE_URL,
   };
 
